89099a24661bd25bf66bcc81f21dcc7d50a112fb
jcfaria <joseclaudio.faria@gmail.com>
2012-07-01 07:38:20 -0300

    Updated 'demo' and 'man' for some functions

--- demo/bpca.R
+++ demo/bpca.R
@@ -310,5 +310,129 @@ plot(bpca(gabriel1971,
      ylim=c(-1,1),
      zlim=c(-1,1))
 
+
+## New options ploting
+data(ontario)
+
+plot(bpca(ontario))
+
+## Labels for all objects
+(obj.lab <- paste('g',
+                  1:18,
+                  sep=''))
+
+# Giving obj.labels
+plot(bpca(ontario),
+    obj.labels=obj.lab) 
+
+# Evaluate an object (1 is the default)
+plot(bpca(ontario),
+     type='eo',
+     obj.cex=1)
+
+plot(bpca(ontario),
+     type='eo',
+     obj.id=7,
+     obj.cex=1)
+
+# Giving obj.labels
+plot(bpca(ontario),
+     type='eo',
+     obj.labels=obj.lab,
+     obj.id=7,
+     obj.cex=1)
+
+# The same as above
+plot(bpca(ontario),
+     type='eo',
+     obj.labels=obj.lab,
+     obj.id='g7',
+     obj.cex=1)
+
+# Evaluate a variable (1 is the default)
+plot(bpca(ontario),
+     type='ev',
+     var.pos=2,
+     var.cex=1)
+
+plot(bpca(ontario),
+     type='ev',
+     var.id='E7',
+     obj.labels=obj.lab,
+     var.pos=1,
+     var.cex=1)
+
+# A complete plot
+cl <- 1:3
+plot(bpca(iris[-5]),
+     type='ev',
+     var.id=1,
+     var.fac=.3,
+     obj.names=FALSE,
+     obj.col=cl[unclass(iris$Species)])
+legend('topleft',
+       legend=levels(iris$Species),
+       text.col=cl,
+       pch=19,
+       col=cl,
+       cex=.9,
+       box.lty=0)   
+
+# Compare two objects (1 and 2 are the default)
+plot(bpca(ontario),
+     type='co')
+
+plot(bpca(ontario),
+     type='co',
+     obj.labels=obj.lab)
+
+plot(bpca(ontario),
+     type='co',
+     obj.labels=obj.lab,
+     obj.id=13:14)
+
+plot(bpca(ontario),
+     type='co',
+     obj.labels=obj.lab,
+     obj.id=c('g7', 'g13'))
+
+# Compare two variables
+plot(bpca(ontario),
+     type='cv')
+
+# Which won where/what
+plot(bpca(ontario),
+     type='ww')
+
+# Discrimitiveness vs. representativeness
+plot(bpca(ontario),
+     type='dv')
+
+# Means vs. stability
+plot(bpca(ontario),
+     type='ms')
+
+# Rank objects with ref. to the ideal variable 
+plot(bpca(ontario),
+     type='ro')
+
+# Rank variables with ref. to the ideal object
+plot(bpca(ontario),
+     type='rv')
+
+plot(bpca(iris[-5]),
+     type='eo',
+     obj.id=42,
+     obj.cex=1)
+
+plot(bpca(iris[-5]),
+     type='ev',
+     var.id='Sepal.Width')
+
+plot(bpca(iris[-5]),
+     type='ev',
+     var.id='Sepal.Width',
+     var.factor=.3)
+
 devAskNewPage(oask)
 
