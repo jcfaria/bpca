dd17a6173d35b5424b86a5e9eade8d49be08134e
jcfaria <joseclaudio.faria@gmail.com>
2013-11-26 06:12:56 -0300

    Version 1.2-2

--- R/var.rdf.R
+++ R/var.rdf.R
@@ -1,20 +1,19 @@
-var.rdf <-
-  function(x,
-           var.rb,
-           limit)
-  {
-    dif <- 100 * abs(var.rb - cor(x))
+var.rdf <- function(x,
+                    var.rb,
+                    limit)
+{
+  dif <- 100 * abs(var.rb - cor(x))
 
-    big <- dif > limit
-    leq <- dif <= limit
+  big <- dif > limit
+  leq <- dif <= limit
 
-    dif[big] <- '*'
-    dif[leq] <- ''
-    diag(dif) <- '-'
+  dif[big] <- '*'
+  dif[leq] <- ''
+  diag(dif) <- '-'
 
-    var.rd <- dif
-    dimnames(var.rd) <- list(dimnames(x)[[2]],
-                             dimnames(x)[[2]])
+  var.rd <- dif
+  dimnames(var.rd) <- list(dimnames(x)[[2]],
+                           dimnames(x)[[2]])
 
-    return(as.data.frame(var.rd))
-  }
+  return(as.data.frame(var.rd))
+}
